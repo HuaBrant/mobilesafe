646906cd94271e5f5a620ae72124803868ed6abf
zwh <1114370689@qq.com>
2017-11-13 11:37:25 +0800

    Initial commit
    Signed-off-by: zwh <1114370689@qq.com>

--- app/src/main/java/com/zwh/mobilesafe/TaskManagerActivity.java
+++ app/src/main/java/com/zwh/mobilesafe/TaskManagerActivity.java
@@ -1,8 +1,11 @@
 package com.zwh.mobilesafe;
 
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.format.Formatter;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -15,6 +18,7 @@ import android.widget.TextView;
 
 import com.zwh.mobilesafe.domain.ProcessInfo;
 import com.zwh.mobilesafe.engine.ProcessInfoProvider;
+import com.zwh.mobilesafe.view.MyToast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,6 +40,8 @@ class TaskManagerActivity extends Activity implements View.OnClickListener {
     //switchover usrApp and systemApp (response Button selectAll and one side had onClick:judge userApp or systemApp
     private Button btUser;
     private Button btSystem;
+    private Button btSelectAll;
+    private Button btKeyClear;
     private ListView lvUsertask;
     private ListView lvSystemtask;
 
@@ -61,19 +67,148 @@ class TaskManagerActivity extends Activity implements View.OnClickListener {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 CheckBox checkBox = (CheckBox) view.findViewById(R.id.cb_taskmanager);
                 ProcessInfo info = (ProcessInfo) lvUsertask.getItemAtPosition(i);
-
+                if (info.getPackname().equals(getPackageName())){
+                    return;
+                }
+                if (info.isChecked()){
+                    info.setChecked(false);
+                    checkBox.setChecked(true);
+                }else {
+                    info.setChecked(true);
+                    checkBox.setChecked(true);
+                }
             }
         });
+        lvSystemtask.setOnItemClickListener(new AdapterView
+                .OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView,
+                                    View view, int i, long l) {
+                CheckBox checkBox = (CheckBox) view.findViewById(R
+                        .id.cb_taskmanager);
+                ProcessInfo info = (ProcessInfo) lvSystemtask
+                        .getItemAtPosition(i);
+                if (info.isChecked()){
+                    info.setChecked(false);
+                    checkBox.setChecked(true);
+                }else {
+                    info.setChecked(true);
+                    checkBox.setChecked(true);
+                }
+            }
+        });
+        btUser.setOnClickListener(this);
+        btUser.setBackground(getResources().getDrawable(R.
+                drawable.bt_pressed));
+        btSystem.setOnClickListener(this);
+        btSystem.setBackground(getResources().getDrawable(R.
+                drawable.bt_normal));
+        btSelectAll.setOnClickListener(this);
+        btKeyClear.setOnClickListener(this);
+        //default entry userView
+        lvSystemtask.setVisibility(View.GONE);
+        userAdapter = new UserAdapter();
+        lvUsertask.setAdapter(userAdapter);
+        tvheader = new TextView(getApplicationContext());
+        tvheader.setText("杀死系统进程会导致系统不稳定");
+        tvheader.setBackgroundColor(Color.YELLOW);
+        lvSystemtask.addHeaderView(tvheader);
+        systemAdapter = new SystemAdapter();
+        lvSystemtask.setAdapter(systemAdapter);
     }
 
     @Override
     public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.bt_user:
+                if (tvheader !=null){
+                    lvSystemtask.removeHeaderView(view);
+                    tvheader =null;
+                }
+                showUserApp =true;
+                btUser.setBackground(getResources().getDrawable(R.
+                        drawable.bt_pressed));
+                btSystem.setBackground(getResources().getDrawable(R.
+                        drawable.bt_normal));
+                lvSystemtask.setVisibility(View.INVISIBLE);
+                lvUsertask.setVisibility(View.VISIBLE);
+                break;
+            case R.id.bt_system:
+                showUserApp =false;
+                btUser.setBackground(getResources().getDrawable(R.
+                        drawable.bt_normal));
+                btSystem.setBackground(getResources().getDrawable(R.
+                        drawable.bt_pressed));
+                lvSystemtask.setVisibility(View.VISIBLE);
+                lvUsertask.setVisibility(View.INVISIBLE);
+                break;
+            case R.id.bt_select_all_task:
+                selectAll(view);
+                break;
+            case R.id.bt_key_clear_task:
+                oneKeyClear(view);
+                break;
+        }
 
+    }
+
+    public void selectAll(View view) {
+        if (showUserApp){
+            for (ProcessInfo info : userProcessInfos){
+                info.setChecked(true);
+                userAdapter.notifyDataSetChanged();
+            }
+        }else {
+            for (ProcessInfo info: systemProcessInfos){
+                info.setChecked(true);
+                userAdapter.notifyDataSetChanged();
+            }
+        }
+    }
+    public void oneKeyClear(View view){
+        ActivityManager activityManager = (ActivityManager)
+                getSystemService(ACTIVITY_SERVICE);
+        int count=0;
+        long memsize =0;
+        List<ProcessInfo> killedProcessInfos = new ArrayList<ProcessInfo>();
+        if (showUserApp){
+            for (ProcessInfo info : userProcessInfos){
+                if (info.isChecked()){
+                    count++;
+                    memsize+=info.getMemsize();
+                    assert activityManager != null;
+                    activityManager.killBackgroundProcesses(info.getPackname());
+                    killedProcessInfos.add(info);
+                }
+            }
+        }else {
+            for (ProcessInfo info : systemProcessInfos) {
+                if (info.isChecked()) {
+                    count++;
+                    memsize += info.getMemsize();
+                    assert activityManager != null;
+                    activityManager.killBackgroundProcesses(info.getPackname());
+                    killedProcessInfos.add(info);
+
+                }
+            }
+        }
+        if (showUserApp){
+            userAdapter.notifyDataSetChanged();
+        }else {
+            systemAdapter.notifyDataSetChanged();
+        }
+        MyToast.showToast(
+                this,
+                "杀死了" + count + "个进程,释放了"
+                        + Formatter.formatFileSize(this, memsize) + "内存");
     }
 
     private void initView() {
         btUser = (Button) findViewById(R.id.bt_user);
         btSystem = (Button) findViewById(R.id.bt_system);
+        btKeyClear= (Button) findViewById(R.id.bt_key_clear_task);
+        btSelectAll = (Button) findViewById(R.id.bt_select_all_task);
         lvUsertask = (ListView) findViewById(R.id.lv_usertask);
         lvSystemtask = (ListView) findViewById(R.id.lv_systemtask);
     }
@@ -102,45 +237,79 @@ class TaskManagerActivity extends Activity implements View.OnClickListener {
                 view = View.inflate(getApplicationContext(), R.layout.task_manager_item, null);
                 holder = new ViewHolder(view);
                 view.setTag(holder);
-
+            }else {
+                view =convertView;
+                holder =(ViewHolder)view.getTag();
             }
-            return null;
+            ProcessInfo info = userProcessInfos.get(i);
+            if (info.getPackname().equals(getPackageName())){
+                holder.cb_manager.setVisibility(View.INVISIBLE);
+            }else {
+                holder.cb_manager.setVisibility(View.VISIBLE);
+            }
+            holder.tv_Mem.setText(Formatter.formatFileSize(getApplicationContext(),
+                    info.getMemsize()));
+            holder.iv_icon.setImageDrawable(info.getIcon());
+            holder.tv_name.setText(info.getAppname());
+            holder.cb_manager.setChecked(info.isChecked());
+            return view;
         }
 
-        private  class ViewHolder {
-            ImageView iv_con;
-            TextView tv_name;
-            TextView tv_Mem;
-            CheckBox cb_manager;
+    }
+    private  static class ViewHolder {
+        ImageView iv_icon;
+        TextView tv_name;
+        TextView tv_Mem;
+        CheckBox cb_manager;
 
-            ViewHolder(View view) {
-                this.iv_con = (ImageView) view.findViewById(R.id.iv_taskmanger_icon);
-                this.tv_name = (TextView) view.findViewById(R.id.tv_taskmanager_appname);
-                this.tv_Mem = (TextView) view.findViewById(R.id.tv_taskmanager_mem);
-                this.cb_manager = (CheckBox) view.findViewById(R.id.cb_taskmanager);
-            }
+        ViewHolder(View view) {
+            iv_icon = (ImageView) view.findViewById(R.id.iv_taskmanger_icon);
+            this.tv_name = (TextView) view.findViewById(R.id.tv_taskmanager_appname);
+            this.tv_Mem = (TextView) view.findViewById(R.id.tv_taskmanager_mem);
+            this.cb_manager = (CheckBox) view.findViewById(R.id.cb_taskmanager);
         }
     }
 
     private class SystemAdapter extends BaseAdapter {
         @Override
         public int getCount() {
-            return 0;
+            return systemProcessInfos.size();
         }
 
         @Override
         public Object getItem(int i) {
-            return null;
+            return systemProcessInfos.get(i);
         }
 
         @Override
         public long getItemId(int i) {
-            return 0;
+            return i;
         }
 
         @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-            return null;
+        public View getView(int i, View convertView, ViewGroup viewGroup) {
+            View view;
+            ViewHolder holder;
+            if (convertView == null) {
+                view = View.inflate(getApplicationContext(), R.layout.task_manager_item, null);
+                holder = new ViewHolder(view);
+                view.setTag(holder);
+            }else {
+                view =convertView;
+                holder =(ViewHolder)view.getTag();
+            }
+            ProcessInfo info = userProcessInfos.get(i);
+            if (info.getPackname().equals(getPackageName())){
+                holder.cb_manager.setVisibility(View.INVISIBLE);
+            }else {
+                holder.cb_manager.setVisibility(View.VISIBLE);
+            }
+            holder.tv_Mem.setText(Formatter.formatFileSize(getApplicationContext(),
+                    info.getMemsize()));
+            holder.iv_icon.setImageDrawable(info.getIcon());
+            holder.tv_name.setText(info.getAppname());
+            holder.cb_manager.setChecked(info.isChecked());
+            return view;
         }
     }
 
